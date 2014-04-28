4aaeae20066063b8eca62cfad14310718ec27215
studhaal <spamstudhaal@gmail.com>
2014-04-28 16:51:53 +0200

    SW7 und SW8

---
+++ SW7/src/sw7/MaxDimensionComparator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sw7;
+
+import java.util.Comparator;
+/**
+ *
+ * @author studhaal
+ */
+public class MaxDimensionComparator implements Comparator<Cube> {
+
+    @Override
+    public int compare(Cube c1, Cube c2) {
+        return c1.getMaxDimension()-c2.getMaxDimension();
+    }
+    
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this==obj)
+            return true;
+        if(this==null)
+            return false;
+        return this.getClass()==obj.getClass();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+    
+}
